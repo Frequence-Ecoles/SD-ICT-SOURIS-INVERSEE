57be3dfd185f1987c735ebae63d36658b6113d5e
louismnt <louis.manotte@gmail.com>
2022-09-12 16:56:10 +0200

    Update
    Asteroids going through earth + rotation + defeat conditions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var cursor = document.getElementById('cursor');
-var cursorX, cursorY;
+var cursorX,
+  cursorY;
 
 var time = 15000;
 var addTime = 2500;
@@ -8,11 +9,16 @@ let objectsArray = [];
 let asteroidX = 40;
 let asteroidY = 80;
 
+let earthX = document.body.clientWidth * 0.50;
+let earthY = document.body.clientHeight * 0.90;
+let earthRayon = 125;
+
 asteroidVelocity = 1;
-velocityMultiplication = asteroidVelocity/10;
+velocityMultiplication = asteroidVelocity / 10;
+let defeat = false;
+
 
 const mainContainer = document.getElementById('mainContainer');
-
 
 // inverted cursor move
 
@@ -23,33 +29,41 @@ document.addEventListener("mousemove", function(e) {
   // cursorY = document.body.clientHeight - e.pageY;
   cursorY = e.pageY;
 
-
   cursor.style.left = cursorX + 'px';
   cursor.style.top = cursorY + 'px';
 
   // console.log(cursorX, cursorY);
 })
 
-const getRandomInt = (min, max) =>  {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
-    };
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 const playableArea = document.getElementById('playable-area');
-//
-// setInterval(function() {
-//  let goPointThis = document.createElement('div');
-//  goPointThis.classList.add('pointThis');
-//  playableArea.appendChild(goPointThis);
-//  goPointThis.addEventListener("click", function() {
-//    this.style.backgroundColor = "blue";
-//  })
-// }, 1000);
 
 let objectsCount = 0;
 let reqAnim;
 
+// set up the detection of distance between two elements
+
+function getPositionAtCenter(element) {
+  const {top, left, width, height} = element.getBoundingClientRect();
+  return {
+    x: left + width / 2,
+    y: top + height / 2
+  };
+}
+
+function getDistanceBetweenElements(a, b) {
+  const aPosition = getPositionAtCenter(a);
+  const bPosition = getPositionAtCenter(b);
+
+  return Math.hypot(aPosition.x - bPosition.x, aPosition.y - bPosition.y);
+}
+
+// define the asteroids objects
 
 class Object {
 
@@ -66,100 +80,119 @@ class Object {
     object.style.left = this.x + "px";
     object.style.top = this.y + "px";
 
-
     playableArea.appendChild(object);
     this.object = object;
 
   }
 
-  detectClick(){
+  detectClick() {
     // if ( 25 > this.x - cursorX > -25 && 25 > this.y - cursorY > -25 ) {
-      console.log(this.id);
-      let asteroidToDelete = document.getElementById('asteroid' + this.id);
-      this.backgroundColor = "blue";
-      asteroidToDelete.classList.add('displayNone');
+    console.log(this.id);
+    let asteroidToDelete = document.getElementById('asteroid' + this.id);
+    this.backgroundColor = "blue";
+    asteroidToDelete.classList.add('displayNone');
     // }
   }
 
-  update(){
+  update() {
+    // definition of direction, by calculating the vector
+    let vectorX = earthX - this.x;
+    let vectorY = earthY - this.y;
+
+    let vectorCoeff = vectorX / vectorY;
+    // calculating angle of the vector
+    var angle = Math.atan2(vectorX, vectorY);
+    var degrees = 180 * angle / Math.PI;
+    let finalAngle = (360 + Math.round(degrees)) % 360;
+    console.log(360 - finalAngle);
+
     let asteroid = document.getElementById('asteroid' + this.id);
-    // this.x+=0.1;
-    this.y += asteroidVelocity+velocityMultiplication;
+    this.x += 1 * vectorCoeff;
+    this.y += 1;
     asteroid.style.left = this.x + "px";
     asteroid.style.top = this.y + "px";
-    // console.log(this.x);
+    asteroid.style.transform = 'rotate(calc(360deg - ' + finalAngle + 'deg))';
 
-    if (asteroidX > this.x - cursorX && this.x - cursorX > -asteroidX
-    && asteroidY > this.y - cursorY && this.y - cursorY > -asteroidY) {
-      // asteroid.classList.add('targeted');
-    } else {
-      // asteroid.classList.remove('targeted');
+    let distance = getDistanceBetweenElements(asteroid, document.getElementById('earth'));
+    if (distance < 150) {
+      // DEFEAT
+      cancelAnimationFrame(reqAnim);
+      clearInterval(timerCounting);
+      clearInterval(createObjects);
+      defeat = true;
 
+      var tasperdu = document.createElement('h1')
+      tasperdu.innerHTML = "PERDU !";
+      tasperdu.classList.add('tasperdu');
+      mainContainer.appendChild(tasperdu);
     }
 
   }
 
 }
 
-function  animate(){
+function animate() {
   // console.log('executed once');
   for (var i = 0; i < objectsArray.length; i++) {
     objectsArray[i].update();
   }
-  reqAnim = requestAnimationFrame(animate)
+  if (defeat) {
+    cancelAnimationFrame(reqAnim);
+  } else {
+    reqAnim = requestAnimationFrame(animate)
+  }
 
 }
 
-
 // creation des objets
+let objectsSpawnSpeed = 3000;
 
-let createObjects = setInterval(function () {
+let createObjects = setInterval(function() {
 
-  objectsArray.push(new Object(getRandomInt(0, document.body.clientWidth - 70), getRandomInt(100, document.body.clientHeight/3), objectsCount));
+  objectsArray.push(new Object(getRandomInt(-250, document.body.clientWidth + 250), -100, objectsCount));
 
   objectsCount++;
-}, 3000);
+  objectsSpawnSpeed += 30;
+}, objectsSpawnSpeed);
 
 reqAnim = requestAnimationFrame(animate)
-
 
 // click trigger
 
 var winCount = 0;
 
-document.addEventListener('click', function(){
+document.addEventListener('click', function() {
   // generation of the explosion
   let explosion = document.createElement('div');
-  explosion. classList.add('explosion');
+  explosion.classList.add('explosion');
   explosion.style.left = cursorX - 25 + "px";
   explosion.style.top = cursorY - 25 + "px";
   mainContainer.appendChild(explosion);
-  window.setTimeout(function(){
+  window.setTimeout(function() {
     explosion.remove();
   }, 560);
 
   // detection of matched asteroids
   for (var i = 0; i < objectsArray.length; i++) {
-    if (asteroidX > objectsArray[i].x - cursorX && objectsArray[i].x - cursorX > -asteroidX
-    && asteroidY > objectsArray[i].y - cursorY && objectsArray[i].y - cursorY > -asteroidY) {
+    if (asteroidX > objectsArray[i].x - cursorX && objectsArray[i].x - cursorX > -asteroidX && asteroidY > objectsArray[i].y - cursorY && objectsArray[i].y - cursorY > -asteroidY) {
 
       // console.log(objectsArray[i].x - cursorX)
       objectsArray[i].object.style.backgroundColor = "blue";
       objectsArray[i].detectClick();
       time += addTime;
       winCount++;
-
-      if (winCount == 10) {
-        // stop animation, timer and object creation
-        cancelAnimationFrame(reqAnim);
-        clearInterval(timerCounting);
-        clearInterval(createObjects);
-
-        var tasgagne = document.createElement('h1')
-        tasgagne.innerHTML = "C'EST GAGNÉ !";
-        tasgagne.classList.add('tasgagne');
-        mainContainer.appendChild(tasgagne);
-      }
+      //
+      // if (winCount == 10) {
+      //    stop animation, timer and object creation
+      //   cancelAnimationFrame(reqAnim);
+      //   clearInterval(timerCounting);
+      //   clearInterval(createObjects);
+      //
+      //   var tasgagne = document.createElement('h1')
+      //   tasgagne.innerHTML = "C'EST GAGNÉ !";
+      //   tasgagne.classList.add('tasgagne');
+      //   mainContainer.appendChild(tasgagne);
+      // }
 
     }
   }
@@ -169,26 +202,16 @@ document.addEventListener('click', function(){
 
 const timer = document.getElementById('timer');
 
+var timerCounting = setInterval(function() {
+  time -= 100;
 
-var timerCounting = setInterval(function () {
-    time -= 100;
+  // timer.innerHTML = "Temps restant : " + time/1000 + "s";
 
-    // timer.innerHTML = "Temps restant : " + time/1000 + "s";
+  if (time <= 0) {
+    // stop animation, timer and object creation
 
-    if (time <=  0) {
-      // stop animation, timer and object creation
-
-      cancelAnimationFrame(reqAnim);
-      console.log
-      clearInterval(timerCounting);
-      clearInterval(createObjects);
-      var tasperdu = document.createElement('h1')
-      tasperdu.innerHTML = "PERDU !";
-      tasperdu.classList.add('tasperdu');
-      mainContainer.appendChild(tasperdu);
-    }
+  }
 
 }, 100);
-
 
 // console.log(objectsArray);
